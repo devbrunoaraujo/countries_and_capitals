e57378c54f4b6b96982d6fd93e390dd2fd08d398
Bruno <dev.brunoaraujo@gmail.com>
2025-05-09 17:03:28 -0300

    bug corrigido

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,80 +2,89 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-
     private $app_data;
 
     public function __construct()
     {
+        // load app_data.php file from app folder
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function showData()
-    {
-        return response()->json($this->app_data);
-    }
-
-    public function startGame():View
+    public function startGame(): View
     {
         return view('home');
     }
 
     public function prepareGame(Request $request)
     {
-        //validaton
+        // validate request
         $request->validate(
             [
-                'total_questions' => 'integer|min:3|max:30|required'
+                'total_questions' => 'required|integer|min:3|max:30'
             ],
             [
-                'total_questions.required' => 'Campo obrigatório!',
-                'total_questions.min' => 'Deve ter no mínimo :min questões!',
-                'total_questions.max' => 'Deve ter no máximo :max questões!',
-                'total_questions.integer' => 'Deve ser um número inteiro!'
+                'total_questions.required' => 'O número de questões é obrigatório',
+                'total_questions.integer' => 'O número de questões tem que ser um valor inteiro',
+                'total_questions.min' => 'No mínimo :min questões',
+                'total_questions.max' => 'No mínimo :max questões',
             ]
         );
-        //Catch number of questions
+
+        // get total questions
         $total_questions = intval($request->input('total_questions'));
+
+        // prepare all the quiz structure
         $quiz = $this->prepareQuiz($total_questions);
 
-        session()->put(
-            [
-                'quiz' => $quiz,
-                'total_questions' => $total_questions,
-                'current_questions' => 1,
-                'correct_answers' => 0,
-                'wrong_answers' => 0
-            ]
-        );
+        // store the quiz in session
+        session()->put([
+            'quiz' => $quiz,
+            'total_questions' => $total_questions,
+            'current_question' => 1,
+            'correct_answers' => 0,
+            'wrong_answers' => 0
+        ]);
 
         return redirect()->route('game');
-
     }
 
     private function prepareQuiz($total_questions)
     {
         $questions = [];
-        $total_contries = count($this->app_data);
-        $indexes = range(0, $total_contries -1);
+        $total_countries = count($this->app_data);
+
+        // create countries index for unique questions
+        $indexes = range(0, $total_countries - 1);
         shuffle($indexes);
         $indexes = array_slice($indexes, 0, $total_questions);
-        $question_number = 1;
 
-        foreach ($indexes as $index) {
+        // create array of questions
+        $question_number = 1;
+        foreach($indexes as $index){
+
             $question['question_number'] = $question_number++;
             $question['country'] = $this->app_data[$index]['country'];
-            $question['capital'] = $this->app_data[$index]['capital'];
-            $other_capitals = array_column($this->app_data, 'capital');
-            shuffle($other_capitals);
-            $question['wrong_answer'] = array_slice($other_capitals, 0 , 3);
-            $question['correct'] = null;
-            $questions[] = $question;
+            $question['correct_answer'] = $this->app_data[$index]['capital'];
 
+            // wrong answers
+            $other_capitals = array_column($this->app_data, 'capital');
+
+            // remove correct answer
+            $other_capitals = array_diff($other_capitals, [$question['correct_answer']]);
+
+            // shuffle the wrong answers
+            shuffle($other_capitals);
+            $question['wrong_answers'] = array_slice($other_capitals, 0, 3);
+
+            // store answer result
+            $question['correct'] = null;
+
+            $questions[] = $question;
         }
 
         return $questions;
@@ -85,8 +94,9 @@ class MainController extends Controller
     {
         $quiz = session('quiz');
         $total_questions = session('total_questions');
-        $current_question = session('current_question') -1;
+        $current_question = session('current_question') - 1;
 
+        // prepare answers to show in view
         $answers = $quiz[$current_question]['wrong_answers'];
         $answers[] = $quiz[$current_question]['correct_answer'];
 
@@ -95,9 +105,8 @@ class MainController extends Controller
         return view('game')->with([
             'country' => $quiz[$current_question]['country'],
             'totalQuestions' => $total_questions,
-            'currentQuestions' => $current_question,
+            'currentQuestion' => $current_question,
             'answers' => $answers
         ]);
     }
-
 }
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,3 +1,36 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+<div class="container">
+
+    <div class="border border-primary rounded-5 p-3 text-center fs-3 mb-3">
+        Pergunta: <span class="text-info fw-bolder">0 / 0</span>
+    </div>
+
+    <div class="text-center fs-3 mb-3">
+        [QUAL É A CAPITAL DE XXXXXX ?]
+    </div>
+
+    <div class="row">
+
+        <div class="col-6 text-center">
+            <p class="response-option">[CAPITAL 1]</p>
+        </div>
+
+        <div class="col-6 text-center">
+            <p class="response-option">[CAPITAL 2]</p>
+        </div>
+
+        <div class="col-6 text-center">
+            <p class="response-option">[CAPITAL 3]</p>
+        </div>
+
+        <div class="col-6 text-center">
+            <p class="response-option">[CAPITAL 4]</p>
+        </div>
+
+    </div>
+
+</div>
+
+<!-- cancel game -->
+<div class="text-center mt-5">
+    <a href="#" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
 </div>
